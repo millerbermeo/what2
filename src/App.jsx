898c52244eebffd607dbebd8401149da9c48a369
millerbermeo <millerbermeo30@gmail.com>
2024-03-14 14:23:12 -0500

    rendimiento

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,49 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Mensajes from './pages/opciones/Mensajes';
-import Perfil from './pages/user/Perfil';
-import Campanas from './pages/opciones/Campanas';
-import Contacto from './pages/user/Contacto';
-import SendCampana from './pages/opciones/SendCampana'
-import Dashboard from './pages/Dashboard';
-import AccesoAgente from './pages/AccesoAgente';
-import PlantillaSaludo from './pages/opciones/PlantillaSaludo';
-import PlantillasTablas from './pages/opciones/PlantillasTablas';
+
+// Importa los componentes de forma diferida
+const Home = React.lazy(() => import('./pages/Home'));
+const Login = React.lazy(() => import('./pages/Login'));
+const Mensajes = React.lazy(() => import('./pages/opciones/Mensajes'));
+const Perfil = React.lazy(() => import('./pages/user/Perfil'));
+const Campanas = React.lazy(() => import('./pages/opciones/Campanas'));
+const Contacto = React.lazy(() => import('./pages/user/Contacto'));
+const SendCampana = React.lazy(() => import('./pages/opciones/SendCampana'));
+const Dashboard = React.lazy(() => import('./pages/Dashboard'));
+const AccesoAgente = React.lazy(() => import('./pages/AccesoAgente'));
+const PlantillaSaludo = React.lazy(() => import('./pages/opciones/PlantillaSaludo'));
+const PlantillasTablas = React.lazy(() => import('./pages/opciones/PlantillasTablas'));
 
 function App() {
-const user = JSON.parse(sessionStorage.getItem('user'));
-const user2 = JSON.parse(sessionStorage.getItem('user2'));
-
+  const user = JSON.parse(sessionStorage.getItem('user'));
+  const user2 = JSON.parse(sessionStorage.getItem('user2'));
 
   const PrivateRoute = ({ element, path }) => {
     if (user && user.type === 'agente') {
-      // Si el usuario es un agente, redirige a la página de inicio
       return element;
     }
-  
     if (user2 && user2.type === 'admin') {
-      // Si el usuario2 es un admin, renderiza la ruta privada
       return element;
     }
-  
-    // En caso de que ninguna condición se cumpla, redirige a la página de inicio de sesión
     return <Navigate to="/" />;
   };
 
   return (
-    <Routes>
-      {/* Ruta pública para el inicio de sesión */}
-      <Route path="/" element={<Login />} />
-
-      {/* Rutas privadas */}
-      <Route path="/home" element={<PrivateRoute element={<Home />} />} />
-      <Route path="/dashboard" element={<PrivateRoute element={<Dashboard />} />} />
-      <Route path="/perfil" element={<PrivateRoute element={<Perfil />} />} />
-      <Route path="/mensajes" element={<PrivateRoute element={<Mensajes />} />} />
-      <Route path="/campanas" element={<PrivateRoute element={<Campanas />} />} />
-      <Route path="/contacto" element={<PrivateRoute element={<Contacto />} />} />
-      <Route path="/send" element={<PrivateRoute element={<SendCampana />} />} />
-      <Route path="/acceso" element={<PrivateRoute element={<AccesoAgente />} />} />
-      <Route path="/plantilla" element={<PrivateRoute element={<PlantillaSaludo />} />} />
-      <Route path="/listar_plantillas" element={<PrivateRoute element={<PlantillasTablas />} />} />
-
-    </Routes>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<PrivateRoute element={<Home />} />} />
+        <Route path="/dashboard" element={<PrivateRoute element={<Dashboard />} />} />
+        <Route path="/perfil" element={<PrivateRoute element={<Perfil />} />} />
+        <Route path="/mensajes" element={<PrivateRoute element={<Mensajes />} />} />
+        <Route path="/campanas" element={<PrivateRoute element={<Campanas />} />} />
+        <Route path="/contacto" element={<PrivateRoute element={<Contacto />} />} />
+        <Route path="/send" element={<PrivateRoute element={<SendCampana />} />} />
+        <Route path="/acceso" element={<PrivateRoute element={<AccesoAgente />} />} />
+        <Route path="/plantilla" element={<PrivateRoute element={<PlantillaSaludo />} />} />
+        <Route path="/listar_plantillas" element={<PrivateRoute element={<PlantillasTablas />} />} />
+      </Routes>
+    </Suspense>
   );
 }
 
